a0520329d10ab85462429c204c6f8fb7b0463de9
MaximGuriev <maxim.guriev@yandex.ru>
2026-03-09 15:00:42 +0500

    PageFactory самостаятельное задание

---
+++ components/courses/create_course_exercises_toolbar_view_component.py
@@ -0,0 +1,27 @@
+from components.base_component import BaseComponent
+from playwright.sync_api import Page, expect
+
+from elements.button import Button
+from elements.text import Text
+
+
+class CreateCourseExercisesToolbarViewComponent(BaseComponent):
+    def __init__(self, page: Page):
+        super().__init__(page)
+
+        self.exercises_title = Text(page, 'courses-list-toolbar-title-text', 'Title')
+        self.create_exercise_button = Button(page,'courses-list-toolbar-create-course-button', 'Button')
+
+
+    def check_visible(self, is_create_course_disabled: bool = True):
+
+        if is_create_course_disabled:
+            self.create_exercise_button.check_enabled()
+
+        if not is_create_course_disabled:
+            self.create_exercise_button.check_disabled()
+
+
+    def click_create_exercise_button(self):
+        self.create_exercise_button.check_visible()
+        self.create_exercise_button.click()
--- components/views/empty_view_component.py
+++ components/views/empty_view_component.py
@@ -10,10 +10,6 @@ class EmptyViewComponent(BaseComponent):
     def __init__(self, page: Page, identifier: str):
         super().__init__(page)
 
-        #self.icon = page.get_by_test_id(f'{identifier}-empty-view-icon')
-        #self.title = page.get_by_test_id(f'{identifier}-empty-view-title-text')
-        #self.description = page.get_by_test_id(f'{identifier}-empty-view-description-text')
-
         self.icon = Icon(page, f'{identifier}-empty-view-icon', 'Icon')
         self.title = Text(page,f'{identifier}-empty-view-title-text', 'Text')
         self.description = Text(page,f'{identifier}-empty-view-description-text', 'Description')
@@ -25,8 +21,4 @@ class EmptyViewComponent(BaseComponent):
         self.description.check_visible()
         self.description.check_have_text(description)
 
-        #expect(self.icon).to_be_visible()
-        #expect(self.title).to_be_visible()
-        #expect(self.title).to_have_text(title)
-        #expect(self.description).to_be_visible()
-        #expect(self.description).to_have_text(description)
+
