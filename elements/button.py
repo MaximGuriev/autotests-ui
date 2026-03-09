484abd2555133033c469f95c2f2a7ad47d236e7e
MaximGuriev <maxim.guriev@yandex.ru>
2026-03-09 14:22:59 +0500

    Практическое применение PageFactory в автотестах

--- elements/button.py
+++ elements/button.py
@@ -2,11 +2,11 @@ from elements.base_elements import BaseElement
 from playwright.sync_api import Page, expect, Locator
 
 class Button(BaseElement):
-    def check_enabled(self, **kwargs):
-        locator = self.get_locator(**kwargs)
+    def check_enabled(self, nth: int = 0, **kwargs):
+        locator = self.get_locator(nth, **kwargs)
         expect(locator).to_be_enabled()
 
 
-    def check_disabled(self, **kwargs):
-        locator = self.get_locator(**kwargs)
+    def check_disabled(self, nth: int = 0, **kwargs):
+        locator = self.get_locator(nth, **kwargs)
         expect(locator).to_be_disabled()
